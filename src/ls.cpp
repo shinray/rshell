f6fd02fe94b63f6874a8006ba825ef120603cde0
Shinray <skuo004@ucr.edu>
2015-02-08 16:13:12 -0800

    progress made to flagcounter

--- src/ls.cpp
+++ src/ls.cpp
@@ -1,6 +1,8 @@
 #include <sys/types.h>
 #include <dirent.h>
 #include <errno.h>
+#include <stdio.h>
+#include <string.h>
 
 #include <iostream>
 
@@ -12,9 +14,51 @@ using namespace std;
  * You MUST add error checking yourself.
  */
 
-int main()
+/* keeps track of arguments/flags */
+bool aflag = false, lflag = false, Rflag = false;
+
+/* searches argv for flags, returns a quick flagcount */
+int flagcounter(int argcc, char **argvv)
 {
+	int numflags = 0; // this actually should count the nonflags aka "files"
+	for (unsigned i = 1; i < argcc; i++)
+	{
+		if (argvv[i][0] == '-')
+		{
+			if (strchr(argv[i],'a') != NULL)
+			{
+				aflag = true;
+				//numflags++;
+			}	
+			if (strchr(argv[i],'l') != NULL)
+			{
+				lflag = true;
+				//numflags++;
+			}	
+			if (strchr(argv[i],'R') != NULL)
+			{
+				Rflag = true;
+				//numflags++;
+			}
+			//break; //done searching?
+		}
+		else // it doesn't start with a '-' therefore not a flag therefore a file!
+		{
+			numflags++;
+		}
+	}
+
+	return numflags;
+}
+
+int main(int argc, char **argv)
+{
+	int numflags = flagcounter(argc, argv);
 	char *dirName = ".";
+	if (numflags < argc - 1)
+	{
+		
+	}
 	DIR *dirp = opendir(dirName);
 	dirent *direntp;
 	while ((direntp == readdir(dirp)))
