03be3d87b1a4a47777e37580454073e23f4901aa
shinray <skuo004@ucr.edu>
2015-02-08 17:49:20 -0800

    progress made to printing function

--- src/ls.cpp
+++ src/ls.cpp
@@ -3,6 +3,9 @@
 #include <errno.h>
 #include <stdio.h>
 #include <string.h>
+#include <string>
+#include <algorithm>
+#include <vector>
 
 #include <iostream>
 
@@ -14,28 +17,29 @@ using namespace std;
  * You MUST add error checking yourself.
  */
 
-/* keeps track of arguments/flags */
-bool aflag = false, lflag = false, Rflag = false;
+/* keeps track of arguments/flags
+bool aflag = false, lflag = false, Rflag = false; */
 
 /* searches argv for flags, returns a quick flagcount */
-int flagcounter(int argcc, char **argvv)
+void checkargs(int argcc, char **argvv, vector<*char> &vfiles, vector<*char> &vdirs, bool &aflag, bool &lflag, bool &Rflag)
 {
-	int numflags = 0; // this actually should count the nonflags aka "files"
+	//int numflags = 0; // this actually should count the nonflags aka "files"
 	for (unsigned i = 1; i < argcc; i++)
 	{
-		if (argvv[i][0] == '-')
+		char *currstring = argvv[i];
+		if (currstring[0] == '-')
 		{
-			if (strchr(argv[i],'a') != NULL)
+			if (strchr(currstring,'a') != NULL)
 			{
 				aflag = true;
 				//numflags++;
 			}	
-			if (strchr(argv[i],'l') != NULL)
+			if (strchr(currstring,'l') != NULL)
 			{
 				lflag = true;
 				//numflags++;
 			}	
-			if (strchr(argv[i],'R') != NULL)
+			if (strchr(currstring,'R') != NULL)
 			{
 				Rflag = true;
 				//numflags++;
@@ -44,21 +48,59 @@ int flagcounter(int argcc, char **argvv)
 		}
 		else // it doesn't start with a '-' therefore not a flag therefore a file!
 		{
-			numflags++;
+			struct stat tempstat;
+			if (stat(currstring, &tempstat) == -1) // syscall here on $currstring
+			{
+				perror("flagcounter: stat: file/directory"); // ALWAYS CALL PERROR AFTER SYSCALLS
+				exit(1);
+			}
+			else if(S_ISDIR(tempstat.st_mode)) // S_ISDIR(*.st_mode) checks if a file is a directory
+			{
+				vdirs.push_back(currstring);
+			}
+			else // is a file
+			{
+				vfiles.push_back(currstring);
+			}
 		}
 	}
+}
 
-	return numflags;
+void printlong(vector<*char> &files, vector<*char> &stats)
+{
+	
+}
+
+void print(char *dir)
+{
+	DIR *dirp = opendir(dir);
+	if (dirp == NULL)
+	{
+		cerr << dir << ':';
+		perror("print: opendir");]
+		exit(1);
+	}
 }
 
 int main(int argc, char **argv)
 {
-	int numflags = flagcounter(argc, argv);
-	char *dirName = ".";
-	if (numflags < argc - 1)
+	vector<*char> vfiles, vfile_stats, vdirs;
+	bool is_hidden = false, is_long = false, is_recursive = false;
+	
+	checkargs(argc, argv, vfiles, vdirs, is_hidden, is_long, is_recursive);
+	
+	if (is_long && !vfiles.empty())
 	{
-		
+		printlong(vfiles, vfile_stats);
 	}
+	else
+	{
+		for(auto i: vfiles)
+		{
+		}
+	}
+	
+	char *dirName = ".";
 	DIR *dirp = opendir(dirName);
 	dirent *direntp;
 	while ((direntp == readdir(dirp)))
